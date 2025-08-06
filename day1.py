0d7e165e57215881e47d1e6b75010c9dd4a0ff6c
python <kushagrasingh001001@gmail.com>
2025-08-06 12:44:30 +0530

    pip , modules and comments

---
+++ day1.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+#comments
+
+
+# ye single line comments
+
+'''ye wale multiple line comments
+isska matalab ham multiple line mei comment likh sakte h
+comments ka use hota hai ki ham code ko explain kare llekin woo line execute nah hu
+'''
+
+#pip are oyu for downloading module and packages

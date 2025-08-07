795126451feb235b19ebdf126d1bfcf0f67e8cf9
python <kushagrasingh001001@gmail.com>
2025-08-07 20:07:24 +0530

    taking user input

---
+++ day5.py
@@ -0,0 +1,13 @@
+# taking input from the user
+
+a = int(input("enter your number :"))
+print(a)
+
+b = str(input("enter your name :"))
+print(b)
+
+c = float(input("enter your number :"))
+print(c)
+
+d = bool(input("enter your number :"))
+print(d)

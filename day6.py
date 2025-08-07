971a4f3cb4ec4253596ca3fc80b5bb4ab1df33e1
python <kushagrasingh001001@gmail.com>
2025-08-07 20:18:23 +0530

    STRINGS ,INDEX AND SLICING

---
+++ day6.py
@@ -0,0 +1,34 @@
+#string 
+
+
+a = "i am khush"
+print(a)
+
+b ='''i am kushagra , 
+student of btech 
+purusing from dsmnru'''
+print(b)
+
+#index of char in str
+
+print(a[2])
+print(a[5])
+
+
+print(b[2])
+print(b[-2])
+print(b[5])
+print(b[-6])
+
+
+#slicing in python
+
+print(a[2:4])
+print(a[:4])
+print(a[2:])
+
+
+
+print(b[2:8])
+print(a[2:-4])
+print(a[:-4])

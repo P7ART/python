f0e4ef4e0b3aa81443187665d2501fd3ba0bd90e
python <kushagrasingh001001@gmail.com>
2025-08-07 17:38:53 +0530

    VARIABLE AND DATATYPE

---
+++ day2.py
@@ -0,0 +1,55 @@
+# variable
+
+a = 556
+b ="khush"
+
+#data type
+
+#int
+a = 556
+
+#float
+b = 556.55
+
+#string
+c = "khush"
+
+#boolean
+d = True
+
+#list
+e = [1,2,3,4,5]
+
+#tuple
+f = (1,2,3,4,5)
+
+#dictionary
+g = {"name": "khush", "age": 20}
+
+#set
+h = {1,2,3,4,5}
+
+#character
+i = 'k'
+
+#print
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+
+#type of variable
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))

f55eb1b59c5b772928b334ed943279db61713249
P7ART <kushagrasingh001001@gmail.com>
2025-08-11 00:42:30 +0530

    string methods

---
+++ day7.py
@@ -0,0 +1,80 @@
+#string methods
+
+a = "i am khush"
+
+print(len(a))
+
+b = "i am younger brother in my family "
+
+print(b.endswith("ily"))
+
+
+c = " i can do it , i am the best , i am the one who can do it without any problem , i am the god of my life"
+
+print(c.count("i"))
+
+
+d = "hello i am kushagra"
+
+print(d.capitalize())
+
+
+e = " i am the best ai/ml engineer in the world"
+
+print(e.split(" "))
+
+
+f ="hell welcome to my world"
+
+print(f.center(50, '*'))
+
+g = "ok how are you ,  i am fine what about you friend"
+
+print(g.find("are"))
+print(g.find("yeah"))
+
+
+h ="i am khushagra , i am the best , i am the one who cannot do it without any problem"
+
+print(h.replace("cannot","can"))
+
+i = "so how was your day my friend i am good"
+
+print(i.upper())
+
+j = " SO TODAY I AM HAPPY BECAUSE I GOT MY NEW LAPTOP"
+print(j.lower())
+ 
+ 
+k = "kushagra !!!!!!"
+
+print(k.strip("!"))
+ 
+l = 'today is a good day'
+print(l.index("good"))
+# print(l.index("bad"))  # This will raise an error if "bad" is not found
+
+
+m = "hello world" 
+print(m.startswith("hello"))
+
+n ="hi there, how are you doing today"
+print(n.title())
+
+o = "i am khushagra , i am the best , i am the one who cannot do it without any problem"
+print(o.splitlines())
+
+p = "happy birthday to you 3"
+print(p.isalnum())
+print(p.isalnum())
+
+q = "happy birthday to you "
+print(q.isalpha()) 
+print(q.isalpha())  
+print(q.isprintable())  
+print(q.islower())  
+print(q.isupper())  
+print(q.istitle())  
+print(q.swapcase())  
+
+ 
